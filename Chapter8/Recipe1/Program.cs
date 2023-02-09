9a2e5741629db8b1f98362306f2384a9088c0f03
유치준 수석 <nerp1.cjyoo@halla.com>
2023-02-09 12:18:15 +0900

    update

---
+++ Chapter8/Recipe1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace Recipe1;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    foreach (int i in EnumerableEventSequence())
+    {
+      Console.Write(i);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("IEnumerable");
+
+    IObservable<int> o = EnumerableEventSequence().ToObservable();
+
+    using (IDisposable subscription = o.Subscribe(Console.Write))
+    {
+      Console.WriteLine();
+      Console.WriteLine("IObservable");
+    }
+
+    o = EnumerableEventSequence().ToObservable().SubscribeOn(TaskPoolScheduler.Default);
+
+    using (IDisposable subscription = o.Subscribe(Console.Write))
+    {
+      Console.WriteLine();
+      Console.WriteLine("IObservable async");
+      Console.ReadLine();
+    }
+  }
+
+  static IEnumerable<int> EnumerableEventSequence()
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      Thread.Sleep(TimeSpan.FromSeconds(0.5));
+      yield return i;
+    }
+  }
+}
---
+++ Chapter8/Recipe2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace Recipe2;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var observer = new CustomObserver();
+    var goodObservable = new CustomSequuence(new[] { 1, 2, 3, 4, 5 });
+    var badObservable = new CustomSequuence(null);
+
+    using (IDisposable subscription = goodObservable.Subscribe(observer))
+    {
+    }
+
+    using (IDisposable subscription = goodObservable.SubscribeOn(TaskPoolScheduler.Default).Subscribe(observer))
+    {
+      Thread.Sleep(100);
+    }
+
+    using (IDisposable subscription = badObservable.SubscribeOn(TaskPoolScheduler.Default).Subscribe(observer))
+    {
+      Console.ReadLine();
+    }
+  }
+
+  private class CustomObserver : IObserver<int>
+  {
+    public void OnCompleted()
+    {
+      Console.WriteLine("Completed");
+    }
+
+    public void OnError(Exception error)
+    {
+      Console.WriteLine("Error: {0}", error.Message);
+    }
+
+    public void OnNext(int value)
+    {
+      Console.WriteLine("Next value: {0}; Thread Id: {1}", value, Thread.CurrentThread.ManagedThreadId);
+    }
+  }
+
+  private class CustomSequuence : IObservable<int>
+  {
+    private readonly IEnumerable<int> _numbers;
+
+    public CustomSequuence(IEnumerable<int> numbers)
+    {
+      _numbers = numbers;
+    }
+
+    public IDisposable Subscribe(IObserver<int> observer)
+    {
+      foreach (var number in _numbers)
+      {
+        observer.OnNext(number);
+      }
+      observer.OnCompleted();
+
+      return Disposable.Empty;
+    }
+  }
+}
